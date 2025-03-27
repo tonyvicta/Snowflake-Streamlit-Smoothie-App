ca99391793e4809746a4a03902276cae1847e4a6
Tony Tawakali <consulting-@outlook.com>
2025-03-27 16:08:52 +0000

    Create 02_create_fruit_options_table.sql

---
+++ snowflake_sql_scripts/02_create_fruit_options_table.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE FRUIT_OPTIONS (
+    FRUIT_ID NUMBER,
+    FRUIT_NAME VARCHAR(25)
+);
+
+
+
+📝 Upload Fruit Data
+
+File: fruits_available_for_smoothies.txt
+Location: @SMOOTHIES.PUBLIC.MY_UPLOAD_FILES
