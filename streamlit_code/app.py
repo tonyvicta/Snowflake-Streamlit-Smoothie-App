6bb7bae1a42ac4c341c183202276b99cd69645c1
Tony Tawakali <consulting-@outlook.com>
2025-04-13 07:42:35 +0100

    Update app.py

--- streamlit_code/app.py
+++ streamlit_code/app.py
@@ -28,24 +28,27 @@ if ingredients_List:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-# Ensure this is somewhere above
 name_on_order = st.text_input("Enter your name")
 
-# st.write(ingredients_string)
+if ingredients_List:
+    ingredients_string = ''
 
-my_insert_stmt = f"""
-    INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-    VALUES ('{ingredients_string}', '{name_on_order}')
-"""
+    for fruit_chosen in ingredients_List:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-# st.write(my_insert_stmt)
+    # Moved inside this block so ingredients_string is defined
+    my_insert_stmt = f"""
+        INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+        VALUES ('{ingredients_string}', '{name_on_order}')
+    """
 
-time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
 
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
 
-st.stop()
-
+    st.stop()
 
