9f47b901716641976fa26e40f286f744fca101c4
Tony Tawakali <consulting-@outlook.com>
2025-04-13 07:24:32 +0100

    Update app.py

--- streamlit_code/app.py
+++ streamlit_code/app.py
@@ -24,7 +24,7 @@ if ingredients_List:
 
    ingredients_string = ''
 
-  for fruit_chosen in ingredients_List:
+   for fruit_chosen in ingredients_List:
       ingredients_string += fruit_chosen + ' '
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
       sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
