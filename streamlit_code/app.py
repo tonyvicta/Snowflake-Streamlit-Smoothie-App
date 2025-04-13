810fe49ea338fa4cfaf7900c838760b8c9e4d429
Tony Tawakali <consulting-@outlook.com>
2025-04-13 18:33:38 +0100

    Update app.py

--- streamlit_code/app.py
+++ streamlit_code/app.py
@@ -12,21 +12,16 @@ st.write('The name of your Smoothie will be:' , name_on_order)
 
 cnx = st.connection ("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options") .select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe. to_pandas ()
-#st. dataframe (pd_df)
-#st.stop()
+pd_df = my_dataframe.to_pandas()
 
 ingredients_List = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe
 )
 
-#st.write(ingredients_string)
 name_on_order = st.text_input("Enter your name")
 
 if ingredients_List:
@@ -46,7 +41,6 @@ for fruit_chosen in ingredients_List:
             st.text(smoothiefroot_response.text[:300])  # Optional debug
     else:
         st.error(f"❌ API call failed with status {smoothiefroot_response.status_code}")
-
 
     my_insert_stmt = f"""
         INSERT INTO smoothies.public.orders (ingredients, name_on_order)
